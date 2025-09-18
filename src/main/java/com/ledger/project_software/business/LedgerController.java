7b49f6a609173f1652f5180078b506b88c7becd3
daiji <jialu.dai@edu.unifi.it>
2025-09-18 23:35:00 +0200

    add rename, copy

--- src/main/java/com/ledger/project_software/business/LedgerController.java
+++ src/main/java/com/ledger/project_software/business/LedgerController.java
@@ -23,14 +23,17 @@ public class LedgerController {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
     @Autowired
     private AccountRepository accountRepository;
+
     @Autowired
     private TransactionRepository transactionRepository;
+
     @Autowired
     private LedgerCategoryRepository ledgerCategoryRepository;
 
-    @PostMapping("/create-ledger")
+    @PostMapping("/create")
     @Transactional
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<String> createLedger(@RequestParam String name,
@@ -40,16 +43,19 @@ public class LedgerController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
         }
 
+        if(ledgerRepository.findByName(name) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Ledger name already exists");
+        }
+
         Ledger ledger=new Ledger(name, owner);
         ledgerRepository.save(ledger);
 
-        //List<Category> templateCategories = categoryRepository.findAll();
         List<Category> templateCategories = categoryRepository.findByParentIsNull();
 
         //copia albero di categorycomponent dal database a ledger
         for (Category template : templateCategories) {
             LedgerCategory ledgerCategory= copyCategory(template, ledger);
-            ledger.addCategoryComponent(ledgerCategory);
+            ledger.getCategories().add(ledgerCategory);
         }
         return ResponseEntity.ok("ledger created successfully");
     }
@@ -57,13 +63,13 @@ public class LedgerController {
     private LedgerCategory copyCategory(Category template, Ledger ledger) {
         LedgerCategory copy;
 
-        if(template.getParent() != null) {
+        if(template.getParent() != null) { //categoria di secondo livello
             copy = new LedgerCategory();
             copy.setName(template.getName());
             copy.setType(template.getType());
             copy.setLedger(ledger);
-            ledger.addCategoryComponent(copy);
-        } else {
+            ledger.getCategories().add(copy);
+        } else { //categoria di primo livello
             copy = new LedgerCategory();
             copy.setName(template.getName());
             copy.setType(template.getType());
@@ -71,7 +77,8 @@ public class LedgerController {
 
             for(Category child : template.getChildren()) {
                 LedgerCategory childCopy = copyCategory(child, ledger);
-                copy.addChild(childCopy);
+                copy.getChildren().add(childCopy);
+                //copy.addChild(childCopy);
                 childCopy.setParent(copy);
             }
         }
@@ -152,6 +159,108 @@ public class LedgerController {
 
         owner.getLedgers().remove(ledger); //rimuove ledger da user
         ledgerRepository.delete(ledger);
-        return ResponseEntity.ok("delete ledger");
+        return ResponseEntity.ok("Ledger deleted successfully");
     }
+
+
+    @PostMapping("/{ledgerId}/copy")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> copyLedger(@PathVariable Long ledgerId,
+                                             Principal principal) {
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        if(ledgerId == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid ledger ID");
+        }
+        Ledger ledger = ledgerRepository.findById(ledgerId).orElse(null);
+        if (ledger == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ledger not found");
+        }
+
+        User owner=userRepository.findByUsername(principal.getName());
+        if (owner == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        if(ledger.getOwner().getId() != owner.getId()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have permission to copy this ledger");
+        }
+
+        Ledger newLedger=new Ledger(ledger.getName()+ " Copy", owner);
+        owner.getLedgers().add(newLedger);
+
+        for (LedgerCategory oldCategory : ledger.getCategories()) {
+            if(oldCategory.getParent() == null) { //solo categorie di primo livello
+                LedgerCategory newCategory = copyLedgerCategory(oldCategory, newLedger);
+                newLedger.getCategories().add(newCategory);
+            }
+
+        }
+
+        ledgerRepository.save(newLedger);
+        return ResponseEntity.ok("copy ledger");
+    }
+    private LedgerCategory copyLedgerCategory(LedgerCategory oldCategory, Ledger newLedger) {
+        LedgerCategory copy;
+
+        if( oldCategory.getParent() != null) { //categoria di secondo livello
+            copy = new LedgerCategory();
+            copy.setName(oldCategory.getName());
+            copy.setType(oldCategory.getType());
+            copy.setLedger(newLedger);
+            newLedger.getCategories().add(copy);
+        } else { //categoria di primo livello
+            copy = new LedgerCategory();
+            copy.setName(oldCategory.getName());
+            copy.setType(oldCategory.getType());
+            copy.setLedger(newLedger);
+
+            for(LedgerCategory child : oldCategory.getChildren()) {
+                LedgerCategory childCopy = copyLedgerCategory(child, newLedger);
+                copy.getChildren().add(childCopy);
+                childCopy.setParent(copy);
+            }
+        }
+        return copy;
+    }
+
+    @PutMapping("/{ledgerId}/rename")
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<String> renameLedger(@PathVariable Long ledgerId,
+                                               @RequestParam String newName,
+                                               Principal principal) {
+        if(principal == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+        Ledger ledger = ledgerRepository.findById(ledgerId).orElse(null);
+        if (ledger == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ledger not found");
+        }
+
+        User owner=userRepository.findByUsername(principal.getName());
+        if (owner == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
+
+        if(ledgerRepository.findByName(newName) != null) {
+            if(ledgerRepository.findByName(newName).getId() != ledgerId) {//se il nome esiste ma appartiene ad un altro ledger
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Ledger name already exists");
+            }else{
+                return ResponseEntity.ok("Ledger renamed successfully");
+            }
+        }
+        if(ledger.getOwner().getId() != owner.getId()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have permission to rename this ledger");
+        }
+        if(newName == null || newName.trim().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ledger name cannot be empty");
+        }
+
+        ledger.setName(newName);
+        ledgerRepository.save(ledger);
+        return ResponseEntity.ok("Ledger renamed successfully");
+    }
+
 }
