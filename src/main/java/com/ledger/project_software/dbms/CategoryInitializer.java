112bc682206d3afc12bbe55653e60e8cb8e5cb17
daiji <jialu.dai@edu.unifi.it>
2025-09-18 22:11:14 +0200

    add new categories

--- src/main/java/com/ledger/project_software/dbms/CategoryInitializer.java
+++ src/main/java/com/ledger/project_software/dbms/CategoryInitializer.java
@@ -16,6 +16,7 @@ public class CategoryInitializer {
     public void init() {
         if (categoryRepository.count() == 0) {
 
+            Category food = new Category("Food", CategoryType.EXPENSE);
             Category transport = new Category("Transport", CategoryType.EXPENSE);
             Category salary = new Category("Salary", CategoryType.INCOME);
             Category freelance = new Category("Freelance", CategoryType.INCOME);
@@ -26,6 +27,7 @@ public class CategoryInitializer {
             Category shopping = new Category("Shopping", CategoryType.EXPENSE);
             Category gifts = new Category("Gifts", CategoryType.EXPENSE);
 
+            categoryRepository.save(food);
             categoryRepository.save(transport);
             categoryRepository.save(salary);
             categoryRepository.save(freelance);
@@ -36,13 +38,28 @@ public class CategoryInitializer {
             categoryRepository.save(shopping);
             categoryRepository.save(gifts);
 
+            Category breakfast = new Category("Breakfast", CategoryType.EXPENSE);
+            Category lunch = new Category("Lunch", CategoryType.EXPENSE);
+            Category dinner = new Category("Dinner", CategoryType.EXPENSE);
+            categoryRepository.save(breakfast);
+            categoryRepository.save(lunch);
+            categoryRepository.save(dinner);
+            food.getChildren().add(breakfast);
+            food.getChildren().add(lunch);
+            food.getChildren().add(dinner);
+            breakfast.setParent(food);
+            lunch.setParent(food);
+            dinner.setParent(food);
+            categoryRepository.save(food);
 
             Category bus = new Category("Bus", CategoryType.EXPENSE);
             Category train = new Category("Train", CategoryType.EXPENSE);
             categoryRepository.save(bus);
             categoryRepository.save(train);
-            transport.addChild(bus);
-            transport.addChild(train);
+            transport.getChildren().add(bus);
+            transport.getChildren().add(train);
+            bus.setParent(transport);
+            train.setParent(transport);
             categoryRepository.save(transport);
 
 
