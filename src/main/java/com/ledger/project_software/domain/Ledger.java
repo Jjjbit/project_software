c665a7c9137d7927ce78949e96bb01f431c4261d
daiji <jialu.dai@edu.unifi.it>
2025-09-18 23:37:05 +0200

    delete BorrowingAndLending, totalIncome, totalExpenses

--- src/main/java/com/ledger/project_software/domain/Ledger.java
+++ src/main/java/com/ledger/project_software/domain/Ledger.java
@@ -21,20 +21,11 @@ public class Ledger {
     @JoinColumn(name = "owner_id")
     private User owner;
 
-    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
+    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Transaction> transactions=new ArrayList<>(); //relazione tra Transaction e Ledger è composizione
 
-    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
+    @OneToMany(mappedBy = "ledger", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LedgerCategory> categories = new ArrayList<>();
-
-    @OneToMany(mappedBy = "ledger", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
-    private List<BorrowingAndLending> borrowingAndLendings = new ArrayList<>(); // relazione tra LoanRecord e Ledger è aggregazione
-
-    @Column(name = "total_income", precision = 15, scale = 2, nullable = false)
-    private BigDecimal totalIncome = BigDecimal.ZERO; // Totale entrate del ledger
-
-    @Column(name = "total_expenses", precision = 15, scale = 2, nullable = false)
-    private BigDecimal totalExpenses = BigDecimal.ZERO; // Totale uscite del ledger
 
     public Ledger() {}
     public Ledger(String name, User owner) {
@@ -43,27 +34,17 @@ public class Ledger {
     }
 
     public String getName(){return this.name;}
+    public void setName(String name){this.name=name;}
     public User getOwner(){return this.owner;}
-    public void addTransaction(Transaction tx) {
-        transactions.add(tx); // Aggiunge una transazione al ledger
-    }
     public List<Transaction> getTransactions() {
         return transactions;
     }
     public List<LedgerCategory> getCategories(){return categories;}
 
-    public void removeTransaction(Transaction tx) {
-        transactions.remove(tx); // Rimuove una transazione dal ledger
-    }
     public Long getId() {
         return id;
     }
 
-
-    public void addCategoryComponent(LedgerCategory category) {
-        categories.add(category);
-        category.setLedger(this);
-    }
 
     //does not matter income or expense
     public List<Transaction> getTransactionsForMonth(YearMonth month) {
