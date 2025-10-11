9efebff01c9c725fdd3e9f01a41da7c873fa585e
daiji <jialu.dai@edu.unifi.it>
2025-10-11 23:58:53 +0200

    add findByOwner

--- src/main/java/com/ledger/project_software/Repository/LedgerRepository.java
+++ src/main/java/com/ledger/project_software/Repository/LedgerRepository.java
@@ -1,10 +1,14 @@
 package com.ledger.project_software.Repository;
 
 import com.ledger.project_software.domain.Ledger;
+import com.ledger.project_software.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LedgerRepository extends JpaRepository<Ledger, Long> {
     Ledger findByName(String name);
+    List<Ledger> findByOwner(User owner);
 }
