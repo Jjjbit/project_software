d4d022500beda237f10945e82158585f84dd81fa
daiji <jialu.dai@edu.unifi.it>
2025-10-11 23:21:12 +0200

    add findByOwner, findByName

--- src/main/java/com/ledger/project_software/Repository/AccountRepository.java
+++ src/main/java/com/ledger/project_software/Repository/AccountRepository.java
@@ -1,10 +1,14 @@
 package com.ledger.project_software.Repository;
 
 import com.ledger.project_software.domain.Account;
+import com.ledger.project_software.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    Account findByName(String name);
+    List<Account> findByOwner(User owner);
 }
