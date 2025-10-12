ea9ce26eec526faf42214d4b07133ff7602932d5
daiji <jialu.dai@edu.unifi.it>
2025-10-12 19:46:10 +0200

    add findByUserAndOptionalCategoryAndPeriod, findActiveCategoryBudget

--- src/main/java/com/ledger/project_software/Repository/BudgetRepository.java
+++ src/main/java/com/ledger/project_software/Repository/BudgetRepository.java
@@ -41,4 +41,29 @@ public interface BudgetRepository extends JpaRepository<Budget, Long> {
                                                  @Param("subCategoryId") Long subCategoryId,
                                                  @Param("today") LocalDate today,
                                                  @Param("period") Budget.Period period);
+
+    //for test
+    @Query("SELECT b FROM Budget b " +
+       "WHERE b.owner.id = :userId " +
+       "AND (:categoryId IS NULL AND b.category IS NULL OR :categoryId IS NOT NULL AND b.category.id = :categoryId) " +
+       "AND b.period = :period")
+    Optional<Budget> findByUserAndOptionalCategoryAndPeriod(@Param("userId") Long userId,
+                                                            @Param("categoryId") Long categoryId,
+                                                            @Param("period") Budget.Period period);
+
+
+    //ritorna il budget attivo di una categoria
+    @Query("SELECT b FROM Budget b " +
+            "WHERE b.owner.id = :userId " +
+            "AND b.category IS NOT NULL " +
+            "AND b.category.parent IS NULL " +
+            "AND b.category.id = :categoryId " +
+            "AND b.period = :period " +
+            "AND :today BETWEEN b.startDate AND b.endDate")
+    Optional<Budget> findActiveCategoryBudget(@Param("userId") Long userId,
+                                              @Param("categoryId") Long categoryId,
+                                              @Param("today") LocalDate today,
+                                              @Param("period") Budget.Period period);
+
 }
+
