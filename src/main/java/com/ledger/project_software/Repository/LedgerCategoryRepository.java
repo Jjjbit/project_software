ac92ba278ec0ea911454772c1b25373eccbc6b41
daiji <jialu.dai@edu.unifi.it>
2025-10-14 17:12:18 +0200

    add findByParentId, findByLedgerIdAndParentIsNull

--- src/main/java/com/ledger/project_software/Repository/LedgerCategoryRepository.java
+++ src/main/java/com/ledger/project_software/Repository/LedgerCategoryRepository.java
@@ -5,8 +5,12 @@ import com.ledger.project_software.domain.LedgerCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LedgerCategoryRepository extends JpaRepository<LedgerCategory, Long> {
     LedgerCategory findByLedgerAndName(Ledger ledger, String name);
     boolean existsByLedgerAndName(Ledger ledger, String name);
+    List<LedgerCategory> findByParentId(Long categoryId);
+    List<LedgerCategory> findByLedgerIdAndParentIsNull(Long ledgerId);
 }
