fb5560f797e3c247dcc266c0964401665d814a79
daiji <jialu.dai@edu.unifi.it>
2025-10-15 18:50:52 +0200

    complete all functional test for Ledger

--- src/test/java/com/ledger/project_software/LedgerTest.java
+++ src/test/java/com/ledger/project_software/LedgerTest.java
@@ -6,21 +6,32 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(classes = com.ledger.project_software.ProjectSoftwareApplication.class)
 @Transactional
@@ -66,6 +77,21 @@ public class LedgerTest {
         userRepository.save(testUser);
 
     }
+
+    @Configuration //conterte String in YearMonth per i test
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            registry.addConverter(new Converter<String, YearMonth>() {
+                @Override
+                public YearMonth convert(String source) {
+                    return YearMonth.parse(source, DateTimeFormatter.ofPattern("yyyy-MM"));
+                }
+            });
+        }
+    }
+
 
     @Test
     @WithMockUser(username = "Alice")
@@ -240,6 +266,155 @@ public class LedgerTest {
 
         Ledger renamedLedger = ledgerRepository.findById(ledger.getId()).orElse(null);
         Assertions.assertEquals("Renamed Ledger", renamedLedger.getName());
+    }
+
+
+    @Test
+    @WithMockUser(username = "Alice")
+    public void testGetAllLedgers() throws Exception {
+        Ledger testLedger = new Ledger("Test Ledger", testUser);
+        ledgerRepository.save(testLedger);
+        testUser.getLedgers().add(testLedger);
+        Ledger testLedger1 = new Ledger("Another Ledger", testUser);
+        ledgerRepository.save(testLedger1);
+        testUser.getLedgers().add(testLedger1);
+
+        mockMvc.perform(get("/ledgers/all-ledgers")
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name").value("Test Ledger"))
+                .andExpect(jsonPath("$[1].name").value("Another Ledger"));
+
+    }
+
+    @Test
+    @WithMockUser(username = "Alice")
+    public void testGetLedgerTransactions() throws Exception {
+        Ledger testLedger = new Ledger("Test Ledger", testUser);
+        ledgerRepository.save(testLedger);
+        testUser.getLedgers().add(testLedger);
+
+        Transaction tx1 = new Transfer(LocalDate.of(2025, 10, 5),
+                null,
+                testAccount1,
+                testAccount2,
+                BigDecimal.valueOf(100),
+                testLedger
+        );
+        transactionRepository.save(tx1);
+        testAccount1.addTransaction(tx1);
+        testAccount2.addTransaction(tx1);
+        testLedger.getTransactions().add(tx1);
+
+        Transaction tx2 = new Transfer(LocalDate.of(2025, 10, 5),
+                null,
+                testAccount2,
+                testAccount1,
+                BigDecimal.valueOf(50),
+                testLedger
+        );
+        transactionRepository.save(tx2);
+        testAccount1.addTransaction(tx2);
+        testAccount2.addTransaction(tx2);
+        testLedger.getTransactions().add(tx2);
+
+        accountRepository.save(testAccount1);
+        accountRepository.save(testAccount2);
+        ledgerRepository.save(testLedger);
+
+        mockMvc.perform(get("/ledgers/{ledgerId}/all-transactions-for-month", testLedger.getId())
+                        .principal(() -> "Alice")
+                        .param("month", "2025-10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].amount").value(100))
+                .andExpect(jsonPath("$[1].amount").value(50));
+
+    }
+
+    @Test
+    @WithMockUser(username = "Alice")
+    public void testGetLedgerCategories() throws Exception {
+        Ledger testLedger = new Ledger("Test Ledger", testUser);
+        ledgerRepository.save(testLedger);
+        testUser.getLedgers().add(testLedger);
+
+        LedgerCategory foodCategory = new LedgerCategory("Food", CategoryType.EXPENSE, testLedger);
+        ledgerCategoryRepository.save(foodCategory);
+        testLedger.getCategories().add(foodCategory);
+
+        LedgerCategory transportCategory = new LedgerCategory("Transport", CategoryType.EXPENSE, testLedger);
+        ledgerCategoryRepository.save(transportCategory);
+        testLedger.getCategories().add(transportCategory);
+
+        LedgerCategory lunchCategory = new LedgerCategory("Lunch", CategoryType.EXPENSE, testLedger);
+        ledgerCategoryRepository.save(lunchCategory);
+        testLedger.getCategories().add(lunchCategory);
+        lunchCategory.setParent(foodCategory);
+        foodCategory.getChildren().add(lunchCategory);
+
+        ledgerCategoryRepository.save(foodCategory);
+        ledgerCategoryRepository.save(transportCategory);
+        ledgerCategoryRepository.save(lunchCategory);
+        ledgerRepository.save(testLedger);
+
+
+        mockMvc.perform(get("/ledgers/{ledgerId}/categories", testLedger.getId())
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.ledgerName").value("Test Ledger"))
+                .andExpect(jsonPath("$.categories", hasSize(2)))
+                .andExpect(jsonPath("$.categories[0].CategoryName").value("Food"))
+                .andExpect(jsonPath("$.categories[1].CategoryName").value("Transport"))
+                .andExpect(jsonPath("$.categories[0].subCategories[0].SubCategoryName").value("Lunch"));;
+    }
+
+    @Test
+    @WithMockUser(username = "Alice")
+    public void testGetMonthlySummary() throws Exception {
+        Ledger testLedger = new Ledger("Test Ledger", testUser);
+        ledgerRepository.save(testLedger);
+        testUser.getLedgers().add(testLedger);
+
+        Transaction tx1=new Expense(LocalDate.of(2025,6,5), BigDecimal.valueOf(320), null, testAccount1,testLedger,null);
+        transactionRepository.save(tx1);
+        testLedger.getTransactions().add(tx1);
+        testAccount1.addTransaction(tx1);
+
+        Transaction tx2=new Income(LocalDate.of(2025,6,5), BigDecimal.valueOf(500), null, testAccount2,testLedger,null);
+        transactionRepository.save(tx2);
+        testLedger.getTransactions().add(tx2);
+        testAccount2.addTransaction(tx2);
+
+
+        Transaction tx3=new Expense(LocalDate.now(), BigDecimal.valueOf(320), null, testAccount1,testLedger,null);
+        transactionRepository.save(tx3);
+        testLedger.getTransactions().add(tx3);
+        testAccount1.addTransaction(tx3);
+
+        Transaction tx4=new Income(LocalDate.now(), BigDecimal.valueOf(500), null, testAccount2,testLedger,null);
+        transactionRepository.save(tx4);
+        testLedger.getTransactions().add(tx4);
+        testAccount2.addTransaction(tx4);
+
+
+        mockMvc.perform(get("/ledgers/{ledgerId}/monthly-summary", testLedger.getId())
+                        .param("month", "2025-06")
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.ledgerName").value("Test Ledger"))
+                .andExpect(jsonPath("$.month").value("2025-06"))
+                .andExpect(jsonPath("$.totalIncome").value(500))
+                .andExpect(jsonPath("$.totalExpense").value(320));
+
+        mockMvc.perform(get("/ledgers/{ledgerId}/monthly-summary", testLedger.getId())
+                        .principal(() -> "Alice"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.ledgerName").value("Test Ledger"))
+                .andExpect(jsonPath("$.month").value(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM"))))
+                .andExpect(jsonPath("$.totalIncome").value(500))
+                .andExpect(jsonPath("$.totalExpense").value(320));
     }
 
 }
