34a219d5ebb3199b443d9df0d34d8c0b676f0566
daiji <jialu.dai@edu.unifi.it>
2025-10-16 00:15:57 +0200

    improve

--- src/test/java/com/ledger/project_software/UserStructuralTest.java
+++ src/test/java/com/ledger/project_software/UserStructuralTest.java
@@ -4,15 +4,12 @@ import com.ledger.project_software.Repository.UserRepository;
 import com.ledger.project_software.business.UserController;
 import com.ledger.project_software.domain.*;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
