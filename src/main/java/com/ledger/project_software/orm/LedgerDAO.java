96edf86a969cdf30a42ef980e271d94f9c015196
daiji <jialu.dai@edu.unifi.it>
2025-10-18 15:17:43 +0200

    add findByNameAndOwne

--- src/main/java/com/ledger/project_software/orm/LedgerDAO.java
+++ src/main/java/com/ledger/project_software/orm/LedgerDAO.java
@@ -3,6 +3,8 @@ package com.ledger.project_software.orm;
 import com.ledger.project_software.domain.Ledger;
 import com.ledger.project_software.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.List;
 @Repository
 public interface LedgerDAO extends JpaRepository<Ledger, Long> {
     Ledger findByName(String name);
-    List<Ledger> findByOwner(User owner);
+
+    @Query("SELECT l FROM Ledger l WHERE l.owner= :owner")
+    List<Ledger> findByOwner(@Param ("owner") User owner);
+
+    Ledger findByNameAndOwner(String name, User owner);
 }
