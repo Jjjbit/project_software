7de0a63ad9a4f8a28da0a52468cdcef5e76acda4
daiji <jialu.dai@edu.unifi.it>
2025-10-20 17:50:06 +0200

    add query findByLinkedAccountId

--- src/main/java/com/ledger/project_software/orm/InstallmentPlanDAO.java
+++ src/main/java/com/ledger/project_software/orm/InstallmentPlanDAO.java
@@ -2,8 +2,14 @@ package com.ledger.project_software.orm;
 
 import com.ledger.project_software.domain.InstallmentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InstallmentPlanDAO extends JpaRepository<InstallmentPlan, Long> {
+    @Query("SELECT ip FROM InstallmentPlan ip " +
+            "WHERE ip.linkedAccount.id = :accountId")
+    List<InstallmentPlan> findByLinkedAccountId(Long accountId);
 }
